87d58a7e6a449d144a19324342c0451549e04c26
Hirak Roy <royhirakp@gmail.com>
2024-03-07 21:00:37 +0530

    email added

--- service/ItemService.js
+++ service/ItemService.js
@@ -45,6 +45,15 @@ class RouteController {
       });
     } catch (error) {
       console.error("Error in createNewItem route:", error);
+
+      if (error.code === 11000) {
+        const duplicateKey = Object.keys(error.keyPattern)[0];
+        return res.status(409).json({
+          status: "error",
+          message: `Duplicate key error. The ${duplicateKey} is already taken.`,
+        });
+      }
+
       // handel error form mongoose model
       if (error.name === "ValidationError") {
         const validationErrors = Object.values(error.errors).map(
@@ -95,8 +104,8 @@ class RouteController {
   //PUT / UPDATE
   static async updateById(req, res) {
     const itemId = req?.params?.id;
-    const updatedData = req?.body;
     try {
+      const updatedData = req?.body;
       if (!updatedData || Object.keys(updatedData).length === 0) {
         return res.status(400).json({
           status: "error",
@@ -144,6 +153,28 @@ class RouteController {
             "Email configuration is missing. Please check enveriment varible.",
         });
       }
+
+      // handel error is data is empty
+      const updatedData = req?.body;
+      if (!updatedData || Object.keys(updatedData).length === 0) {
+        return res.status(400).json({
+          status: "error",
+          message: "Request body is empty or invalid.",
+        });
+      }
+      if (!Array.isArray(updatedData?.data)) {
+        return res.status(400).json({
+          status: "error",
+          message: "invaid input , data not a array.",
+        });
+      } else if (updatedData.data.length === 0) {
+        return res.status(400).json({
+          status: "error",
+          message: "Request body is empty or invalid. deta array is empty",
+        });
+      }
+      let data = req?.body?.data || [];
+
       // send the otp to the user mail
       const transporter = nodemailer.createTransport({
         service: "gmail",
@@ -153,18 +184,83 @@ class RouteController {
         },
       });
 
+      let tableHead = `  <thead>
+      <tr style="border-bottom: 1px solid #ddd; background: #f2f2f2">
+        <th style="padding: 12px; text-align: left; border: 1px solid #ddd">
+          Name
+        </th>
+        <th style="padding: 12px; text-align: left; border: 1px solid #ddd">
+          Phone number
+        </th>
+        <th style="padding: 12px; text-align: left; border: 1px solid #ddd">
+          Email
+        </th>
+        <th style="padding: 12px; text-align: left; border: 1px solid #ddd">
+          Hobbies
+        </th>
+      </tr>
+    </thead>`;
+
+      let tableString = ``;
+      data.map((item, i) => {
+        tableString =
+          tableString +
+          `
+      <tbody>
+      <tr style="border-bottom: 1px solid #ddd">
+        <td style="padding: 12px; text-align: left; border: 1px solid #ddd">
+          ${item.name}
+        </td>
+        <td style="padding: 12px; text-align: left; border: 1px solid #ddd">
+        ${item.phone}
+        </td>
+        <td style="padding: 12px; text-align: left; border: 1px solid #ddd">
+        ${item.email}
+        </td>
+        <td style="padding: 12px; text-align: left; border: 1px solid #ddd">
+        ${item.hobbies}
+        </td>
+      </tr>
+    </tbody>
+      `;
+      });
+
+      let email_InString =
+        `
+        <h1>Assignment for RedPositive Service OPC</h1>
+        <h2>Table data exported</h2>
+        <p>Assignment by: HIRAK ROY</p>
+  
+        <a href="https://www.linkedin.com/in/royhirakp">LinkedIN</a>
+        <br />
+        <a href=" https://github.com/royhirakp">gitHub</a
+        ><br />
+  
+        <a href="mailto:hirakroy@mail.com">hirakroy@mail.com</a
+        ><br />
+        <a href="mailto:royhirakp@gmail.com">royhirakp@gmail.com</a
+        >
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <h1>RESULT:</h1>
+
+
+          <table style="border-collapse: collapse; width: 100%">` +
+        tableHead +
+        tableString +
+        `</table>
+    `;
+
       await transporter.sendMail(
         {
           from: "royhiark@gmail.com", // sender address
           to: "royhirakp@gmail.com", // list of receivers
           subject: "Hotel Booking page , OTP MAIL",
           text: ` OTP MAIL mail`, // plain text body
-          html: ` <body style="font-family: system-ui, math, sans-serif">
-                  <div>
-                    Assignment for RedPositive Service OPC 
-                    <br />                     
-                  </div>
-                </body>`, // html body
+          html: email_InString, // html body
         },
         // error insending mail
         (error, info) => {
